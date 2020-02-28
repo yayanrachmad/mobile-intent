d19e352ebe4ed8d8f5cb4cc53c11e8ebed80530c
yayanrachmad <yayanshare@gmail.com>
2020-02-28 10:27:48 +0700

    mobile intent

--- app/src/main/java/id/ac/polinema/intent/ExplicitIntentActivity.java
+++ app/src/main/java/id/ac/polinema/intent/ExplicitIntentActivity.java
@@ -1,15 +1,35 @@
 package id.ac.polinema.intent;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ExplicitIntentActivity extends AppCompatActivity {
 
+    private EditText usernameInput;
+    private TextView outputText;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_explicit_intent);
+        //  Deklarasi attribut untuk findViewById
+        usernameInput = findViewById(R.id.input_name);
+        outputText = findViewById(R.id.text_output);
+    }
+
+    //Onclick button dari main
+    public void handleSubmit(View view) {
+//        Input
+        String name = usernameInput.getText().toString();
+//        Output
+        outputText.setText(name);
+
     }
 
 }
--- app/src/main/java/id/ac/polinema/intent/ProfileBundleActivity.java
+++ app/src/main/java/id/ac/polinema/intent/ProfileBundleActivity.java
@@ -1,10 +1,20 @@
 package id.ac.polinema.intent;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import static id.ac.polinema.intent.BundleActivity.AGE_KEY;
+import static id.ac.polinema.intent.BundleActivity.NAME_KEY;
+import static id.ac.polinema.intent.BundleActivity.USERNAME_KEY;
+
 public class ProfileBundleActivity extends AppCompatActivity {
+
+    private TextView usernameText;
+    private TextView nameText;
+    private TextView ageText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,10 +22,24 @@ public class ProfileBundleActivity extends AppCompatActivity {
         setContentView(R.layout.activity_profile_bundle);
 
         // TODO: bind here
+        usernameText = findViewById(R.id.text_username);
+        nameText = findViewById(R.id.text_name);
+        ageText = findViewById(R.id.text_age);
 
         Bundle extras = getIntent().getExtras();
+
+        String usernameValue = extras.getString("username");
+        String nameValue = extras.getString("name");
+        int ageValue = extras.getInt("age");
+
+//        Get Text dari Bind
         if (extras != null) {
             // TODO: display value here
+            usernameText.setText(usernameValue);
+            nameText.setText(nameValue);
+            ageText.setText(String.valueOf(ageValue));
+
         }
     }
+
 }
